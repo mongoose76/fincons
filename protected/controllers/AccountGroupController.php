93342e2872d10615da93d3edfa3533b3901408de
Paraschiv Doru <doru@snowglobegames.com>
2013-04-05 19:54:10 +0300

    removed some auto generated messages

--- protected/controllers/AccountGroupController.php
+++ protected/controllers/AccountGroupController.php
@@ -37,10 +37,7 @@ class AccountGroupController extends Controller
 			array('allow', // allow admin user to perform 'admin' and 'delete' actions
 				'actions'=>array('admin','delete'),
 				'users'=>array('admin'),
-			),
-			array('deny',  // deny all users
-				'users'=>array('*'),
-			),
+			)
 		);
 	}
 
